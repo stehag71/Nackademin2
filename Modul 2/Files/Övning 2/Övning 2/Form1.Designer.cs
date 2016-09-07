f83936c33ebc9d6a5b6ef028fc774f9490c744d2
Stefan Haglund <stehag71@gmail.com>
2016-09-07 15:43:22 +0200

    commit

---
+++ Modul 2/Files/Övning 2/Övning 2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Övning_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.cmdVisaFiler = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(35, 46);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(369, 20);
+            this.txtFolder.TabIndex = 0;
+            this.txtFolder.Text = "C:\\Users\\Stefan\\Source\\Repos\\Nackademin2\\Modul 2\\Files\\Övning 2";
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(32, 30);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(52, 13);
+            this.lblFolder.TabIndex = 1;
+            this.lblFolder.Text = "Filkatalog";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(35, 72);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(369, 160);
+            this.lstFiles.TabIndex = 2;
+            // 
+            // cmdVisaFiler
+            // 
+            this.cmdVisaFiler.Location = new System.Drawing.Point(319, 18);
+            this.cmdVisaFiler.Name = "cmdVisaFiler";
+            this.cmdVisaFiler.Size = new System.Drawing.Size(85, 22);
+            this.cmdVisaFiler.TabIndex = 3;
+            this.cmdVisaFiler.Text = "Visa filer";
+            this.cmdVisaFiler.UseVisualStyleBackColor = true;
+            this.cmdVisaFiler.Click += new System.EventHandler(this.cmdVisaFiler_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(443, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 160);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(548, 252);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Öppna";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(640, 252);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Spara";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 350);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmdVisaFiler);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button cmdVisaFiler;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Modul 2/Files/Övning 2/Övning 2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Övning_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdVisaFiler_Click(object sender, EventArgs e)
+        {
+            //DirectoryInfo dir = new DirectoryInfo(txtFolder.Text);
+
+            //lstFiles.DataSource = dir.GetFiles();
+
+            string path = txtFolder.Text;
+            string[] files = Directory.GetFiles(path);
+            
+            foreach (var file in files)
+	        {
+                //lstFiles.Items.Add(file);
+                lstFiles.Items.Add(Path.GetFileName(file));
+	        }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+
+        }
+    }
+}
